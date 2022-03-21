d39ae81b33c1f7d8e17b2a766a09f06aed32c118
Jéssica Gelsdorf <82670472+mejessica@users.noreply.github.com>
2022-03-21 10:33:56 -0300

    Update MinhaPlaylist.java

--- learning/MinhaPlaylist.java
+++ learning/MinhaPlaylist.java
@@ -3,25 +3,30 @@ import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
 
-public class MinhaPlaylist{
-    ArrayList<String> nameMusicas = new ArrayList <String>();
-    public static void main(String[] args) {
-        
+public class MinhaPlaylist{ //assinatura da classe
+    ArrayList<String> nameMusicas = new ArrayList <String>(); //declaraçao da coleçao
+    public static void main(String[] args) { //metodo principal de java, assinatura padrao
+        new MinhaPlaylist() .leMusicas(); //instancia objeto 
     }
-    public void adicionaMusica(String musica){
+    public void adicionaMusica(String musica){ 
         String[] lista = musica.split("/");
             nameMusicas.add(lista[0]); //split: quando encontra a "/", ele passa para a linha debaixo
     }
+    public void go(){
+        leMusicas();
+        System.out.println(nameMusicas);
+    }
+    
     public void leMusicas(){
         try{
-            File arquivo = new File ("Listas_Musicas");
-            BufferedReader reader = new BufferedReader(new FileReader(arquivo)); 
+            File arquivo = new File ("MinhaPlaylist.txt");
+            BufferedReader reader = new BufferedReader(new FileReader(arquivo)); //lê e armazena 
             String linha=null;
             while((linha=reader.readLine())!=null){
                 adicionaMusica(linha);
 
             }
-        } catch(Exception ex){
+        } catch(Exception ex){ //gera um erro
             ex.printStackTrace();
         }
     } 
