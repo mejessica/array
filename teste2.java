30d1701956d4d9acb143d7920af2b5d95757d42d
Jéssica Gelsdorf <82670472+mejessica@users.noreply.github.com>
2022-04-25 11:02:36 -0300

    Create teste2.java

---
+++ teste2.java
@@ -0,0 +1,15 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class array2 {
+	public static void main (String[] args) {
+		List<String>ListaAluno = new ArrayList<String>();
+		ListaAluno.add("a");
+		ListaAluno.add("b");
+		ListaAluno.add("c");
+		ListaAluno.add("d");
+		ListaAluno.add("e");
+		System.out.println(ListaAluno);
+	}
+}
