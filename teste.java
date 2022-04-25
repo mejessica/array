8cf49efcab8730497d55e436565cd74577e6d62b
Jéssica Gelsdorf <82670472+mejessica@users.noreply.github.com>
2022-04-25 10:48:36 -0300

    Create teste.java

---
+++ teste.java
@@ -0,0 +1,22 @@
+package atividade;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class array {
+
+public static void main (String [] args) {
+    final int max = 2000; 
+    long tInicio = System.currentTimeMillis();
+    List<Integer> lista= new ArrayList <Integer> ();
+    
+        for(int i = 0; i<max; i++){
+            lista.add(i); //percorre a lista e add
+        }
+        for (int i =0; i<max; i++){
+            lista.contains(i); // percorre a lista vendo o que contem na lista/array
+        }
+        long tFim=System.currentTimeMillis(); //tempo que demorou para pecorrer o algoritmo
+        System.out.println("Tempo total; "+ (tFim - tInicio));
+}
+}
